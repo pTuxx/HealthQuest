42125f6e2a7a84ae62e6ab7d5ccb8c78e47037cd
pTux <55540562+pTuxx@users.noreply.github.com>
2023-12-04 08:01:29 -0500

    ff
    vff

--- submit.php
+++ submit.php
@@ -7,14 +7,14 @@ $queries = array();
 parse_str($_SERVER['QUERY_STRING'], $queries);
 
 $posts = [
-    [
-        "name" => $queries['name'],
-        "urgency" => $queries['urgency'],
-        "points" => json_decode($queries['points'])
-    ]
+    "name" => $queries['name'],
+    "urgency" => $queries['urgency'],
+    "points" => json_decode($queries['points'])
 ];
 
 array_push($db,$posts);
+
+array_multisort(array_column($db, 'urgency'), SORT_DESC, $db);
 
 $json = json_encode($db, JSON_PRETTY_PRINT);
 
